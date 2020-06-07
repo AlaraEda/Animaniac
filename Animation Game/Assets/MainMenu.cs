4fde688c7b3cf16bf86af1edc5cd80f006986aa1
AlaraEdda <0931871@hr.nl>
2020-06-08 01:43:27 +0200

    Added Scenes
    I am now able to switch between various scenes. I have a game Menu which leads to playing the game. And I have a Quit button. In the game I also have buttons that lead back to the Menu scene and that also quits the game.

---
+++ Animation Game/Assets/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Wat je moet gebruiken als je wilt switchen tussen scenes.
+
+public class MainMenu : MonoBehaviour{
+
+    public void PlayGame()
+    {
+        //Ga direct naar de Main/Game
+        SceneManager.LoadScene("Main");
+
+        /*
+         * Een andere optie is SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+         * Je begint vanaf Menu, die zit op plaats 0
+         * Load next level in the que. Je load dus de Play-Screen/MainScreen.
+         * 
+         * Dan ga je ook naar die scene namelijk. Met +1, zeg je eigenlijk dat je naar de volgende
+         * scene wilt gaan door het in je file --> build settings te slepen.
+        */
+    }
+
+    public void DrawScreen()
+    {
+        SceneManager.LoadScene("Drawer");
+    }
+
+    public void DrawToLife()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Verander naam Menu() naar terug.
+    public void Menu()
+    {
+        //Ga vanaf de PlayScreen/Mainscreen terug naar de Menu.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit(); //Ingebouwde quit functie van unity
+    }
+}
